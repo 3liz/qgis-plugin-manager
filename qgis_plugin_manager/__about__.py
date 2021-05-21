ce15d4d4308e2ceb8c293ea7e16733456255de96
Etienne Trimaille <gustrimaille@yahoo.fr>
2021-05-22 00:13:15 +0700

    Add CI, about etc

---
+++ qgis_plugin_manager/__about__.py
@@ -0,0 +1,43 @@
+"""
+Metadata about the package to easily retrieve information about it.
+See: https://packaging.python.org/guides/single-sourcing-package-version/
+"""
+
+from datetime import date
+
+__all__ = [
+    "__author__",
+    "__copyright__",
+    "__email__",
+    "__license__",
+    "__summary__",
+    "__title__",
+    "__title_clean__",
+    "__uri__",
+    "__version__",
+    "__version_info__",
+]
+
+__author__ = "Étienne Trimaille"
+__copyright__ = "2021 - {0}, {1}".format(date.today().year, __author__)
+__email__ = "etrimaille@3liz.com"
+__license__ = "GNU General Public License v3.0"
+__summary__ = "Tool for downloading/managing QGIS plugins from CLI."
+__title__ = "QGIS Plugin Manager"
+__title_clean__ = "".join(e for e in __title__ if e.isalnum())
+__uri__ = "https://github.com/Gustry/qgis-plugin-manager"
+
+# This string might be updated on CI on runtime with a proper semantic version name with X.Y.Z
+__version__ = "__VERSION__"
+
+if "." not in __version__:
+    # If __version__ is still not a proper semantic versioning with X.Y.Z
+    # let's hardcode 0.0.0
+    __version__ = "0.0.0"
+
+__version_info__ = tuple(
+    [
+        int(num) if num.isdigit() else num
+        for num in __version__.replace("-", ".", 1).split(".")
+    ]
+)
--- setup.py
+++ setup.py
@@ -1,14 +1,11 @@
-import os
 import sys
+from pathlib import Path
 
 from setuptools import setup
 
-python_min_version = (3, 7)
+from qgis_plugin_manager import __about__
 
-if os.getenv('CI') == 'true':
-    VERSION = "__VERSION__"
-else:
-    VERSION = "0.0.0"
+python_min_version = (3, 6)
 
 if sys.version_info < python_min_version:
     sys.exit(
@@ -18,22 +15,50 @@ if sys.version_info < python_min_version:
         )
     )
 
+# This string might be updated on CI on runtime with a proper semantic version name with X.Y.Z
+VERSION = "__VERSION__"
+
+if "." not in VERSION:
+    # If VERSION is still not a proper semantic versioning with X.Y.Z
+    # let's hardcode 0.0.0
+    VERSION = "0.0.0"
+
+read_me = Path(__file__).parent.joinpath("README.md").read_text()
+
 setup(
     name="qgis-plugin-manager",
+    author=__about__.__author__,
+    author_email=__about__.__email__,
+    description=__about__.__summary__,
     packages=["qgis_plugin_manager"],
-    entry_points={"console_scripts": ["qgis-plugin-manager = qgis_plugin_manager.cli:main"]},
+    long_description=read_me,
+    long_description_content_type="text/markdown",
+    url=__about__.__uri__,
+    entry_points={"console_scripts": ["qgis-plugin-manager = qgis_plugin_manager.__main__:main"]},
     version=VERSION,
-    description="Manage QGIS plugin when you are on a server.",
-    author="Etienne Trimaille",
-    author_email="etrimaille@3liz.com",
-    # url="https://github.com/opengisch/qgis-plugin-ci",
-    # download_url="https://github.com/opengisch/qgis-plugin-ci/archive/{}.tar.gz".format(VERSION),
+    project_urls={
+        "Docs": __about__.__uri__,
+        "Bug Reports": "{}issues/".format(__about__.__uri__),
+        "Source": __about__.__uri__,
+    },
+    download_url="https://github.com/Gustry/qgis-plugin-manager/archive/{}.tar.gz".format(
+        VERSION
+    ),
     keywords=["QGIS"],
     classifiers=[
-        "Topic :: Scientific/Engineering :: GIS",
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: System Administrators",
+        "Intended Audience :: Developers",
         "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Scientific/Engineering :: GIS",
     ],
     install_requires=[],
     python_requires=">={vmaj}.{vmin}".format(
